e09c1cec4985ef2c47f7aba164844c2ad940c53d
b4010229 <b4010229@my.shu.ac.uk>
2018-01-17 14:28:30 +0000

    Added data samples for Course objects

---
+++ enrolSystem/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,39 @@
+package com.enrol
+
+class Course {
+
+String department
+
+String title
+
+String leader
+
+String code
+
+String studyMode
+
+Date startDate
+
+Date endDate
+
+String description
+
+int numberOfStudents
+
+Double tuitionFees
+
+    static constraints = {
+
+title blank:false, nullable:false
+department blank:false, nullable:false
+leader blank:false, nullable:false
+code blank:false, nullable:false
+numberOfStudents blank:false, nullable:false, max:60, min:20
+startDate blank:false, nullable:false
+endDate blank:false, nullable:false
+studyMode blank:false, nullable:false, size:1..20
+description blank:false, nullable:false, maxSize:5000, widget:'textarea'
+tuitionFees blank:false, nullable:false, amountscale:2
+
+    }
+}
--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,9 +1,46 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { servletContext -> 
+
+def computing=new Course(
+department:'Computing',
+code:'CS123',
+title:'BSc Hon Computing',
+leader:'Dr Michelle Murphy',
+startDate:new Date('09/09/2018'),
+endDate:new Date('07/07/2022'),
+numberOfStudents:55,
+studyMode:'Fulltime',
+tuitionFees:9000.60,
+description:'Lorem ipsum dolor sit amet, cum sale error tantas ne').save()
+
+def pasties=new Course(
+department:'Pasty Baking',
+code:'PB123',
+title:'BSc Pasty Honors',
+leader:'Dr Gregg',
+startDate:new Date('09/09/2018'),
+endDate:new Date('07/07/2022'),
+numberOfStudents:55,
+studyMode:'Fulltime',
+tuitionFees:9000.60,
+description:'Our vision is to become customers favourite for food on-the-go!').save()
+
+def english=new Course(
+department:'English',
+code:'EN123',
+title:'English with Honors',
+leader:'Dr Phil',
+startDate:new Date('09/09/2018'),
+endDate:new Date('07/07/2022'),
+numberOfStudents:55,
+studyMode:'Fulltime',
+tuitionFees:9000.60,
+description:'i dnt read 2 good').save()
+
     }
     def destroy = {
-    }
+greggs     }
 }
